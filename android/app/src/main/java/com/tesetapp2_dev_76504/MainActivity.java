3207cc5dbef05b59eb8bda52f90ea84da4b7fb9c
Crowdbotics <engineering@crowdbotics.com>
2023-04-13 18:56:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tesetapp2_dev_76504/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tesetapp2_dev_76504;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tesetapp2_dev_76504";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tesetapp2-dev-76504",
+  url: "https://tesetapp2-dev-76504.botics.co",
+  api: "https://tesetapp2-dev-76504.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
